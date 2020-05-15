3417d5fe94d6cdd46805ee5dc3bec96083434210
Ian Spivey <ispivey@cloudflare.com>
2020-05-15 06:56:36 -0500

    Adds `metroCode` to `CfRequestProperties`

--- index.d.ts
+++ index.d.ts
@@ -120,6 +120,10 @@ interface CfRequestProperties {
   httpProtocol: string
   latitude?: number
   longitude?: number
+  /**
+   * DMA metro code from which the request was issued, e.g. "635"
+   */
+  metroCode?: number
   postalCode?: string
   /**
    * e.g. "Texas"
