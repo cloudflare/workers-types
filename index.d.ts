4654c23c562c44f15b6f20f2046f43c197ad858e
Florian Klampfer <mail@qwtel.com>
2020-10-04 11:54:57 +0700

    Add jsdoc for scheduled eve nt

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,23 @@ interface FetchEvent {
 }
 
 interface ScheduledEvent {
+  /**
+   * The type of event. This will always return `"scheduled"`.
+   */
   type: string;
+  /**
+   * The time the `ScheduledEvent` was scheduled to be executed in 
+   * milliseconds since January 1, 1970, UTC. 
+   * It can be parsed as `new Date(event.scheduledTime)`
+   */
   scheduledTime: number;
+  /**
+   * Use this method to notify the runtime to wait for asynchronous tasks 
+   * (e.g. logging, analytics to third-party services, streaming and caching). 
+   * The first `event.waitUntil` to fail will be observed and recorded as the 
+   * status in the Cron Trigger Past Events table. Otherwise, it will be 
+   * reported as a Success.
+   */
   waitUntil(promise: Promise<any>): void;
 }
 
