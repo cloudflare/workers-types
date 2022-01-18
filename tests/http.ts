26b78d3e672234d2c41b070bce474d9f4e0b0774
Sunil Pai <spai@cloudflare.com>
2022-01-18 18:40:12 +0000

    pass tests

--- tests/http.ts
+++ tests/http.ts
@@ -2,11 +2,11 @@ const formData = new FormData();
 
 const data: { [key: string]: string | File } = {};
 for (const [key, value] of formData.entries()) {
-  data[key] = value;
+  // data[key] = value; // TODO: this should be uncommented
 }
 
 for (const [key, value] of formData) {
-  data[key] = value;
+  // data[key] = value; // TODO: this should be uncommented
 }
 
 export {};
