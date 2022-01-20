db292c45990789be860847e81f69ee4b3a953b43
Sunil Pai <spai@cloudflare.com>
2022-01-20 13:17:02 +0000

    Deleted a couple of stray comments.

--- tests/http.ts
+++ tests/http.ts
@@ -2,11 +2,11 @@ const formData = new FormData();
 
 const data: { [key: string]: string | File } = {};
 for (const [key, value] of formData.entries()) {
-  data[key] = value; // TODO: this should be uncommented
+  data[key] = value;
 }
 
 for (const [key, value] of formData) {
-  data[key] = value; // TODO: this should be uncommented
+  data[key] = value;
 }
 
 export {};
