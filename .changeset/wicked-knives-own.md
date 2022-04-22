b96c23cb53502a6a6828efb93266e00d99734264
Greg Brimble <gbrimble@cloudflare.com>
2022-04-22 13:49:34 +0100

    Update pages.d.ts (#222)

---
+++ .changeset/wicked-knives-own.md
@@ -0,0 +1,5 @@
+---
+"@cloudflare/workers-types": minor
+---
+
+feature: Adds the `PagesPluginFunction` type and internal `functionPath` to Functions' context.
--- manual-ts/pages.d.ts
+++ manual-ts/pages.d.ts
@@ -2,6 +2,7 @@ type Params<P extends string = any> = Record<P, string | string[]>;
 
 type EventContext<Env, P extends string, Data> = {
   request: Request;
+  functionPath: string;
   waitUntil: (promise: Promise<any>) => void;
   next: (input?: Request | string, init?: RequestInit) => Promise<Response>;
   env: Env & { ASSETS: { fetch: typeof fetch } };
@@ -14,3 +15,23 @@ declare type PagesFunction<
   Params extends string = any,
   Data extends Record<string, unknown> = Record<string, unknown>
 > = (context: EventContext<Env, Params, Data>) => Response | Promise<Response>;
+
+type EventPluginContext<Env, P extends string, Data, PluginArgs> = {
+  request: Request;
+  functionPath: string;
+  waitUntil: (promise: Promise<any>) => void;
+  next: (input?: Request | string, init?: RequestInit) => Promise<Response>;
+  env: Env & { ASSETS: { fetch: typeof fetch } };
+  params: Params<P>;
+  data: Data;
+  pluginArgs: PluginArgs;
+};
+
+declare type PagesPluginFunction<
+  Env = unknown,
+  Params extends string = any,
+  Data extends Record<string, unknown> = Record<string, unknown>,
+  PluginArgs = unknown
+> = (
+  context: EventPluginContext<Env, Params, Data, PluginArgs>
+) => Response | Promise<Response>;
