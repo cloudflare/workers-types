3a9101974a3fae7446a1b3541103e9648467eecd
Greg Brimble <gbrimble@cloudflare.com>
2022-04-22 14:39:27 +0100

    Manually update index.d.ts with the Pages Plugins types

--- index.d.ts
+++ index.d.ts
@@ -1959,3 +1959,23 @@ declare type PagesFunction<
   Params extends string = any,
   Data extends Record<string, unknown> = Record<string, unknown>
 > = (context: EventContext<Env, Params, Data>) => Response | Promise<Response>;
+
+type EventPluginContext<Env, P extends string, Data, PluginArgs> = {
+  request: Request;
+  functionPath: string;
+  waitUntil: (promise: Promise<any>) => void;
+  next: (input?: Request | string, init?: RequestInit) => Promise<Response>;
+  env: Env & { ASSETS: { fetch: typeof fetch } };
+  params: Params<P>;
+  data: Data;
+  pluginArgs: PluginArgs;
+};
+
+declare type PagesPluginFunction<
+  Env = unknown,
+  Params extends string = any,
+  Data extends Record<string, unknown> = Record<string, unknown>,
+  PluginArgs = unknown
+> = (
+  context: EventPluginContext<Env, Params, Data, PluginArgs>
+) => Response | Promise<Response>;
