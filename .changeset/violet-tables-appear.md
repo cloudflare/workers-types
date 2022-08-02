6cac15126701c53b7d988fd4d3dab76d5755b6ce
Glen Maddern <glen83@gmail.com>
2022-08-02 18:29:48 +0100

    Added D1Database and related types (#265)
    Co-authored-by: Glen Maddern <glen@cloudflare.com>

---
+++ .changeset/violet-tables-appear.md
@@ -0,0 +1,5 @@
+---
+"@cloudflare/workers-types": minor
+---
+
+Added D1 binding types
---
+++ overrides/d1.d.ts
@@ -0,0 +1,25 @@
+declare type D1Result<T = unknown> = {
+  results?: T[];
+  lastRowId: number | null;
+  changes: number;
+  duration: number;
+  error?: string;
+};
+declare abstract class D1Database {
+  prepare(query: string): D1PreparedStatement;
+  dump(): Promise<ArrayBuffer>;
+  batch<T = unknown>(statements: D1PreparedStatement[]): Promise<D1Result<T>[]>;
+  exec<T = unknown>(query: string): Promise<D1Result<T>>;
+  _send<T = unknown>(
+    endpoint: string,
+    query: any,
+    params: any[]
+  ): Promise<D1Result<T>[] | D1Result<T>>;
+}
+declare abstract class D1PreparedStatement {
+  bind(...values: any[]): D1PreparedStatement;
+  first<T = unknown>(colName?: string): Promise<T>;
+  run<T = unknown>(): Promise<D1Result<T>>;
+  all<T = unknown>(): Promise<D1Result<T[]>>;
+  raw<T = unknown>(): Promise<T[]>;
+}
